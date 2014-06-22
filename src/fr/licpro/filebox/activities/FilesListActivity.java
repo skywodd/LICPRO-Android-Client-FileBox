c25adeaa28ccbcae8ec6f3f01c42076fed6f81a1
Skywodd <skywodd@gmail.com>
2014-06-22 18:10:06 +0200

    Add stub for all list and detail views

---
+++ src/fr/licpro/filebox/activities/FilesListActivity.java
@@ -0,0 +1,47 @@
+/*
+ * This file is part of LICPRO-Android-Client-FileBox.
+ *
+ * LICPRO-Android-Client-FileBox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LICPRO-Android-Client-FileBox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LICPRO-Android-Client-FileBox.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package fr.licpro.filebox.activities;
+
+import android.os.Bundle;
+import android.util.Log;
+import fr.licpro.filebox.R;
+
+/**
+ * Files list activity.
+ * 
+ * @author skywodd
+ */
+public class FilesListActivity extends AbstractBaseActivity {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.i(LOGCAT_TAG, "FilesListActivity::onCreate()");
+
+		/* Set content view */
+		setContentView(R.layout.activity_files_list);
+	}
+
+	// TODO all display 
+	
+}
