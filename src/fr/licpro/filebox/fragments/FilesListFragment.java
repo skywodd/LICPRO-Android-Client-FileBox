4342e37264c484cb2151d3a95235a48f6d0f1d20
Skywodd <skywodd@gmail.com>
2014-06-22 18:33:40 +0200

    Commit stub for fragments. Go back to work dimitri.

---
+++ src/fr/licpro/filebox/fragments/FilesListFragment.java
@@ -0,0 +1,30 @@
+/*
+ * This file is part of LICPRO-Android-Client-FileBox.
+ *
+ * LICPRO-Android-Client-FileBox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LICPRO-Android-Client-FileBox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LICPRO-Android-Client-FileBox.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package fr.licpro.filebox.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Files list fragment. Display a list of FileboxEntry for the specified
+ * directory.
+ * 
+ * @author skywodd
+ */
+public class FilesListFragment extends Fragment {
+
+}
