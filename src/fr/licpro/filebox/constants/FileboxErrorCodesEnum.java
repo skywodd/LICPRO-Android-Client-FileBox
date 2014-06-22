a39a02751b4cd3ecea5749fb051d9d92b7be4423
Skywodd <skywodd@gmail.com>
2014-06-22 12:15:39 +0200

    Add i18n on error messages

--- src/fr/licpro/filebox/constants/FileboxErrorCodesEnum.java
+++ src/fr/licpro/filebox/constants/FileboxErrorCodesEnum.java
@@ -17,6 +17,11 @@
 
 package fr.licpro.filebox.constants;
 
+import org.apache.commons.lang3.StringUtils;
+
+import android.content.Context;
+import fr.licpro.filebox.R;
+
 /**
  * Enumeration of all possible error codes from the FileBox server. Each error
  * code is composed of an integer error code, an error message (human-readable)
@@ -30,40 +35,40 @@ public enum FileboxErrorCodesEnum {
 	 * Triggered when a bad request is received and no processing of this
 	 * request is possible.
 	 */
-	BAD_REQUEST("40000", "bad.request", "Bad request"),
+	BAD_REQUEST(40000, "bad.request", R.string.error_bad_request),
 
 	/**
 	 * Triggered when the specified user cannot be found in the user database.
 	 */
-	USER_NOT_FOUND("40401", "user.not.found", "User not found"),
+	USER_NOT_FOUND(40401, "user.not.found", R.string.error_user_not_found),
 
 	/**
 	 * Triggered when the given authentication token cannot be validated by the
 	 * server.
 	 */
-	TOKEN_INVALID("40402", "token.invalid", "Token invalid"),
+	TOKEN_INVALID(40402, "token.invalid", R.string.error_token_invalid),
 
 	/**
 	 * Triggered when the specified file handle cannot be found on the server
 	 * files system.
 	 */
-	FILE_NOT_FOUND("40403", "file.not.found", "File not found"),
+	FILE_NOT_FOUND(40403, "file.not.found", R.string.error_file_not_found),
 
 	/**
 	 * Triggered when the given login already exist in the user database.
 	 */
-	LOGIN_ALREADY_EXIST("60001", "login.exist", "Login exist"),
+	LOGIN_ALREADY_EXIST(60001, "login.exist", R.string.error_login_exist),
 
 	/**
 	 * Triggered when an unknown error is detected during the request processing
 	 * at server side.
 	 */
-	UNKNOWN_ERROR("50001", "unknown.error", "Unknown error");
+	UNKNOWN_ERROR(50001, "unknown.error", R.string.error_unknown_error);
 
 	/**
 	 * The error code.
 	 */
-	private final String mErrorCode;
+	private final int mErrorCode;
 
 	/**
 	 * The error message key (machine-readable).
@@ -71,9 +76,9 @@ public enum FileboxErrorCodesEnum {
 	private final String mErrorMessageKey;
 
 	/**
-	 * The error message (human-readable).
+	 * The error message resource ID (human-readable).
 	 */
-	private final String mErrorDefaultMessage;
+	private final int mErrorMessageResId;
 
 	/**
 	 * Constructor of an FileboxErrorCodesEnum element.
@@ -83,13 +88,13 @@ public enum FileboxErrorCodesEnum {
 	 * @param errorMessageKey
 	 *            The error message key (machine-readable).
 	 * @param errorDefaultMessage
-	 *            The error message (human-readable).
+	 *            The error message resource ID (human-readable).
 	 */
-	private FileboxErrorCodesEnum(final String errorCode,
-			final String errorMessageKey, final String errorDefaultMessage) {
+	private FileboxErrorCodesEnum(final int errorCode,
+			final String errorMessageKey, final int errorMessageResId) {
 		mErrorMessageKey = errorMessageKey;
 		mErrorCode = errorCode;
-		mErrorDefaultMessage = errorDefaultMessage;
+		mErrorMessageResId = errorMessageResId;
 	}
 
 	/**
@@ -97,8 +102,36 @@ public enum FileboxErrorCodesEnum {
 	 * 
 	 * @return The error code.
 	 */
-	public String getErrorCode() {
+	public int getErrorCode() {
 		return mErrorCode;
+	}
+
+	/**
+	 * Get the FileboxErrorCodesEnum for the specified error code.
+	 * 
+	 * @param errorCode
+	 *            The error code to search for.
+	 * @return The FileboxErrorCodesEnum associated with the given error code,
+	 *         or null if not found.
+	 */
+	public static FileboxErrorCodesEnum getError(int errorCode) {
+
+		/* Error unknown by default */
+		FileboxErrorCodesEnum result = null;
+
+		/* For each known error */
+		for (FileboxErrorCodesEnum type : FileboxErrorCodesEnum.values()) {
+
+			/* If the error code matches */
+			if (type.mErrorCode == errorCode) {
+
+				/* Store the result */
+				result = type;
+			}
+		}
+
+		/* Return the result */
+		return result;
 	}
 
 	/**
@@ -111,12 +144,56 @@ public enum FileboxErrorCodesEnum {
 	}
 
 	/**
-	 * Get the error message (human-readable).
+	 * Get the FileboxErrorCodesEnum for the specified error message key.
 	 * 
-	 * @return The error message (human-readable).
+	 * @param errorMessageKey
+	 *            The error message key to search for.
+	 * @return The FileboxErrorCodesEnum associated with the given error message
+	 *         key, or null if not found.
 	 */
-	public String getErrorDefaultMessage() {
-		return mErrorDefaultMessage;
+	public static FileboxErrorCodesEnum getError(String errorMessageKey) {
+
+		/* Error unknown by default */
+		FileboxErrorCodesEnum result = null;
+
+		/*
+		 * If the error message key is not blank (not null, not empty, not
+		 * whitespace-only)
+		 */
+		if (StringUtils.isNotBlank(errorMessageKey)) {
+
+			/* For each known error */
+			for (FileboxErrorCodesEnum type : FileboxErrorCodesEnum.values()) {
+
+				/* If the extensions matches */
+				if (type.mErrorMessageKey.equalsIgnoreCase(errorMessageKey)) {
+
+					/* Store the result */
+					result = type;
+				}
+			}
+		}
+
+		/* Return the result */
+		return result;
+	}
+
+	/**
+	 * Get the error message (human-readable and localized).
+	 * 
+	 * @return The error message (human-readable and localized).
+	 */
+	public String getErrorMessage(Context context) {
+		return context.getString(mErrorMessageResId);
+	}
+
+	/**
+	 * Get the error message resource ID (human-readable).
+	 * 
+	 * @return The error message resource ID (human-readable).
+	 */
+	public int getErrorMessageResourceID() {
+		return mErrorMessageResId;
 	}
 
 }
